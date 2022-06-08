e5437e40045590722ae01c11c892c6b3fe9645b2
jonkis12 <104092969+jonkis12@users.noreply.github.com>
2022-06-08 16:59:29 -0400

    Delete Taller-Meca-nico-Rayo-Makween/TallerMecanico directory

--- Taller-Meca-nico-Rayo-Makween/TallerMecanico/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Contacto
-
-class ContactoForm(ModelForm):
-    class Meta:
-        model = Contacto
-        fields = ['rut','nombre','correo','mensaje','categoria']
--- Taller-Meca-nico-Rayo-Makween/TallerMecanico/usuario.sql
+++
@@ -1,5 +0,0 @@
-drop user prueba CASCADE;
-
-create user prueba IDENTIFIED by duoc
-QUOTA UNLIMITED on users;
-GRANT CREATE SESSION, RESOURCE to prueba;
